41d5d78df550a506eb9bc3c4c5118ae81fa66622
bgidley <bgidley@a4272e60-8c34-11de-82e5-a1837a67dc72>
2009-06-17 14:34:30 +0000

    Added selenium test to verify headers actually work

--- tapestry-commons/tapestry-cacheControl/src/test/java/uk/co/ioko/tapestry/cacheControl/integration/CacheControlIntegrationTest.java
+++ tapestry-commons/tapestry-cacheControl/src/test/java/uk/co/ioko/tapestry/cacheControl/integration/CacheControlIntegrationTest.java
@@ -20,27 +20,52 @@
 package uk.co.ioko.tapestry.cacheControl.integration;
 
 import org.apache.tapestry5.test.AbstractIntegrationTestSuite;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by IntelliJ IDEA. User: ben Date: Jun 16, 2009 Time: 9:31:22 AM
  */
-public class CacheControlIntegrationTest  extends AbstractIntegrationTestSuite {
+public class CacheControlIntegrationTest extends AbstractIntegrationTestSuite {
 	public CacheControlIntegrationTest() {
-			super("src/test/webapp");
-		}
+		super("src/test/webapp");
+	}
 
-		@Test
-		public void testFlashLoader() throws Exception {
-			open(BASE_URL + "CacheControlTestPage");
+	@Test
+	public void testCachedPage() throws Exception {
+		open(BASE_URL + "CacheControlTestPage");
 
-			// Get the contents of the date time field
+		// Get the contents of the date time field
+		String date = getText("//dl[@id='live']/dt[2]");
+
+		// Wait 2 seconds
+		Thread.sleep(2000);
+
+		// Refresh the page it shouldn't have changed
+		open(BASE_URL + "CacheControlTestPage");
+
+		this.assertText("//dl[@id='live']/dt[2]", date);
 
 
-			// Wait 2 seconds
-			Thread.sleep(2000);
+	}
 
-			// Refresh the page it shouldn't have changed
+	@Test
+	public void testNotCachedPage() throws Exception {
+		open(BASE_URL + "NotCachedTestPage");
 
-		}
+		// Get the contents of the date time field
+		String date = getText("//dl[@id='live']/dt[2]");
+
+		// Wait 2 seconds
+		Thread.sleep(2000);
+
+		// Refresh the page it shouldn't have changed
+		open(BASE_URL + "CacheControlTestPage");
+
+		String date2 = getText("//dl[@id='live']/dt[2]");
+
+		Assert.assertTrue(!date2.equals(date));
+
+
+	}
 }
