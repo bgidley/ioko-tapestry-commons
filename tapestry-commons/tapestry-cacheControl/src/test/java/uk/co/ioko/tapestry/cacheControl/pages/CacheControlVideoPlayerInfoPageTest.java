b40d6dce3793261d97159eb8ebafeede77f51388
prash.talk <prash.talk@a4272e60-8c34-11de-82e5-a1837a67dc72>
2010-01-12 17:25:14 +0000

    Added Test for VideoPlayerInfo Page

---
+++ tapestry-commons/tapestry-cacheControl/src/test/java/uk/co/ioko/tapestry/cacheControl/pages/CacheControlVideoPlayerInfoPageTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2009 ioko365 Ltd
+ *
+ * This file is part of ioko tapestry-commons.
+ *
+ *     ioko tapestry-commons is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     ioko tapestry-commons is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with ioko tapestry-commons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.co.ioko.tapestry.cacheControl.pages;
+
+import org.apache.tapestry5.dom.Document;
+import org.apache.tapestry5.test.PageTester;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ */
+public class CacheControlVideoPlayerInfoPageTest {
+
+	@Test
+	public void testDisabledAjaxEvents(){
+		PageTester pageTester = new PageTester("uk.co.ioko.tapestry.cacheControl", "Test", "src/test/webapp");
+		Document page1 = pageTester.renderPage("CacheControlVideoPlayerInfoTestPage");
+
+
+
+		Assert.assertNotNull(page1);
+	}
+}
